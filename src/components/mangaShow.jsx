9e9379f682bc97faf942cd575b4c0ac1530623d8
Mohammad Hifzan <misterj435@gmail.com>
2024-12-17 07:14:38 +0530

    added comment functionality

--- src/components/mangaShow.jsx
+++ src/components/mangaShow.jsx
@@ -1,6 +1,7 @@
 import { useParams, Link } from "react-router-dom"
 import React, { useState, useEffect } from 'react';
 import {get} from '../common/utility/toolbox' 
+import Comment from '../common/comment'
 
 function MangaShow(){
 	const { id } = useParams()
@@ -89,86 +90,14 @@ function MangaShow(){
                 <div className="section-title">
                   <h5>List Name</h5>
                 </div>
-                <div style={{maxHeight: 600, overflow: 'scroll'}}>
+                <div style={{maxHeight: '600px', overflowY: 'auto', overflowX: 'hidden'}}>
                   {chaptersList ? chaptersList : <p>No Chapters</p> }
                 </div>
               </div>
             </div>
           </div>
           <div className="row">
-            <div className="col-lg-8 col-md-8">
-              <div className="anime__details__review">
-                <div className="section-title">
-                  <h5>Reviews</h5>
-                </div>
-                <div className="anime__review__item">
-                  <div className="anime__review__item__pic">
-                    <img src="/theme/img/anime/review-1.jpg" alt="" />
-                  </div>
-                  <div className="anime__review__item__text">
-                    <h6>Chris Curry - <span>1 Hour ago</span></h6>
-                    <p>whachikan Just noticed that someone categorized this as belonging to the genre
-                    "demons" LOL</p>
-                  </div>
-                </div>
-                <div className="anime__review__item">
-                  <div className="anime__review__item__pic_reply">
-                    <img src="/theme/img/anime/review-2.jpg" alt="" />
-                  </div>
-                  <div className="anime__review__item__text">
-                    <h6>Chris Curry - <span>1 Hour ago</span></h6>
-                    <p>whachikan Just noticed that someone categorized this as belonging to the genre
-                    "demons" LOL</p>
-                  </div>
-                </div>
-                <div className="anime__review__item">
-                  <div className="anime__review__item__pic">
-                    <img src="/theme/img/anime/review-3.jpg" alt="" />
-                  </div>
-                  <div className="anime__review__item__text">
-                    <h6>Louis Tyler - <span>20 Hour ago</span></h6>
-                    <p>Where is the episode 15 ? Slow update! Tch</p>
-                  </div>
-                </div>
-                <div className="anime__review__item">
-                  <div className="anime__review__item__pic">
-                    <img src="/theme/img/anime/review-4.jpg" alt="" />
-                  </div>
-                  <div className="anime__review__item__text">
-                    <h6>Chris Curry - <span>1 Hour ago</span></h6>
-                    <p>whachikan Just noticed that someone categorized this as belonging to the genre
-                    "demons" LOL</p>
-                  </div>
-                </div>
-                <div className="anime__review__item">
-                  <div className="anime__review__item__pic">
-                    <img src="/theme/img/anime/review-5.jpg" alt="" />
-                  </div>
-                  <div className="anime__review__item__text">
-                    <h6>Lewis Mann - <span>5 Hour ago</span></h6>
-                    <p>Finally it came out ages ago</p>
-                  </div>
-                </div>
-                <div className="anime__review__item">
-                  <div className="anime__review__item__pic">
-                    <img src="/theme/img/anime/review-6.jpg" alt="" />
-                  </div>
-                  <div className="anime__review__item__text">
-                    <h6>Louis Tyler - <span>20 Hour ago</span></h6>
-                    <p>Where is the episode 15 ? Slow update! Tch</p>
-                  </div>
-                </div>
-              </div>
-              <div className="anime__details__form">
-                <div className="section-title">
-                  <h5>Your Comment</h5>
-                </div>
-                <form action="#">
-                  <textarea placeholder="Your Comment"></textarea>
-                  <button type="submit"><i className="fa fa-location-arrow"></i> Review</button>
-                </form>
-              </div>
-            </div>
+            {<Comment />}
             <div className="col-lg-4 col-md-4">
               <div className="anime__details__sidebar">
                 <div className="section-title">
