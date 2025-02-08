2e30039f77929f6f3036e2fb6ec42eb3e938037b
Mohammad Hifzan <misterj435@gmail.com>
2025-02-08 11:59:51 +0530

    worked on adding correct process of fetching from dotenv

--- src/common/utility/toolbox.jsx
+++ src/common/utility/toolbox.jsx
@@ -1,7 +1,7 @@
 import {getCsrf} from './csrf'
 import axios from 'axios';
 import store from '../../user_auths/store'
-const API_URL = process.env.BACKEND_URL;
+const API_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const post = async (path, data) => {
 	const csrfToken = await getCsrf(); // Get CSRF token
