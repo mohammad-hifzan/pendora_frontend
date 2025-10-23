c0cb7c31c88c78f5e45168fd903d0cf356af0799
Mohammad Hifzan <misterj435@gmail.com>
2025-10-23 21:19:31 +0530

    chapter show fixes

--- src/components/chapters.jsx
+++ src/components/chapters.jsx
@@ -152,7 +152,7 @@ const getChapter = async (mangaId, id) => {
 const getChapters = async (manga_id) => {
   try {
     const response = await get(`v2/mangas/${manga_id}/chapters`);
-    return response.data.chapters;
+    return response.data.data;
   } catch (error) {
     console.error("Error fetching mangas:", error);
     throw error;
